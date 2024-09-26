c8ad5f59477406c3a6222259547dc574a7db764d
NguyenHoangTai <nht0391@gmail.com>
2024-09-26 22:24:43 +0700

    final 2

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ let slideIndex = 0;
 const slides = document.querySelector('.slides');
 const totalSlides = document.querySelectorAll('.slides img').length;
 
-function showSlides() {
+function showSlides(index) {
+    slides.style.transform = `translateX(${-index * 100}%)`;
+}
+
+document.querySelector('.arrow-right').addEventListener('click', () => {
     slideIndex++;
     if (slideIndex >= totalSlides) {
         slideIndex = 0;
     }
-    slides.style.transform = `translateX(${-slideIndex * 100}%)`;
-}
-
-setInterval(showSlides, 4000); // Chuyển slide sau mỗi 4 giây
+    showSlides(slideIndex);
+});
